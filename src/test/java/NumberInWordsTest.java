cda3bad963e48d167b58df2526c752f9f84ce75d
twayn <Twayn@ya.ru>
2018-07-15 17:19:03 +0500

    Tests rework

--- src/test/java/NumberInWordsTest.java
+++ src/test/java/NumberInWordsTest.java
@@ -3,23 +3,30 @@ import org.junit.Test;
 
 public class NumberInWordsTest {
 	@Test
-	public void OneThousandMasculineNominativeTest(){
+	public void oneThousandAndOneMasculineNominativeTest(){
 		NumberInWords numberInWords = new NumberInWords();
-		String oneThousand = numberInWords.sumProp(1000L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
-		Assert.assertEquals("одна тысяча", oneThousand);
+		String oneThousandAndOne = numberInWords.sumProp(1001L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
+		Assert.assertEquals("одна тысяча один", oneThousandAndOne);
 	}
 
 	@Test
-	public void SixMasculineNominativeTest(){
+	public void sixMasculineNominativeTest(){
 		NumberInWords numberInWords = new NumberInWords();
 		String six = numberInWords.sumProp(6L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
 		Assert.assertEquals("шесть", six);
 	}
 
 	@Test
-	public void TwelveMasculineNominativeTest(){
+	public void twelveMasculineNominativeTest(){
 		NumberInWords numberInWords = new NumberInWords();
-		String twelve = numberInWords.sumProp(6L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
+		String twelve = numberInWords.sumProp(12L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
 		Assert.assertEquals("двенадцать", twelve);
+	}
+
+	@Test
+	public void twoHundredAndSeventeenMasculineNominativeTest(){
+		NumberInWords numberInWords = new NumberInWords();
+		String twoHundredAndSeventeen = numberInWords.sumProp(217L, INumberInWords.MASCULINE, INumberInWords.NOMINATIVE);
+		Assert.assertEquals("двести семнадцать", twoHundredAndSeventeen);
 	}
 }
