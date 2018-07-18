868a6f7b0e8a886df2640a71df8bc11eec955101
twayn <Twayn@ya.ru>
2018-07-18 23:47:27 +0500

    Text mistakes fixed

--- src/main/java/numbersinwords/NumberInWords.java
+++ src/main/java/numbersinwords/NumberInWords.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class NumberInWords implements INumberInWords {
 	private final static String TOO_BIG_ERR_MSG = "Обрабатываемое число должно быть меньше ";
 	private final static String TOO_SMALL_ERR_MSG = "Обрабатываемое число должно быть больше ";
-	private final static String GENDER_NOT_SPECIFIED = "Обрабатываемое число должно быть больше ";
-	private final static String CASE_NOT_SPECIFIED = "Обрабатываемое число должно быть больше ";
+	private final static String GENDER_NOT_SPECIFIED = "Род не указан ";
+	private final static String CASE_NOT_SPECIFIED = "Падеж не указан ";
 	private final static String WRONG_GENDER = "Неверное значение пола: ";
 	private final static String WRONG_CASE = "Неверное значение падежа: ";
 	private final static String VALID_GENDERS_MSG = "Допустимые значения пола: ";
